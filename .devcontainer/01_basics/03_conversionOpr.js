47c19be0c18fd9fcca74b5a07dcf9170f841e83f
Sandeep Kumar Upadhyay <upadhyayjirajujio@gmail.com>
2025-03-24 09:13:25 +0000

    typeconversion

---
+++ .devcontainer/01_basics/03_conversionOpr.js
@@ -0,0 +1,26 @@
+//let score = 33
+//console.log(typeof score);
+//console.log(typeof (score));
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+//null value get 0;
+// undefined value get Nan;
+//valueInNumber get NaN;
+
+
+// Sir video convesion
+//"33" == 33
+//"33abc" ==  NaN
+//true == 1; false = 0;
+//"" == false
+
+//let isLoggedIn = 1;
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
