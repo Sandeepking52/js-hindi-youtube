7bb2caa706a37b7c98e652008b6d86c168fe7432
Sandeep Kumar Upadhyay <upadhyayjirajujio@gmail.com>
2025-03-24 08:33:11 +0000

    datatype

---
+++ .devcontainer/01_basics/02_datatype.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all js code as newer version
+//alert(3+3)// we are using node js not brower 
+//code readability should be high
+let name = "Sandeep"
+let age = 23
+let isLoggedIn = false
+let state = null
+
+
+// number => 2 to power 53
+// big int used hogh risk
+// string used =  ""
+//Boolean == trye / false
+// null = standAlon value
+//undderfinerd  = not value
+// symbol == unique
+// object == deatial
+console.log(typeof"snadeep")
