0ce3edb99847a07fe28417fbab23bab1031ae8d8
Sandeep Kumar Upadhyay <upadhyayjirajujio@gmail.com>
2025-03-25 12:47:14 +0000

    datatypesummary

---
+++ .devcontainer/01_basics/datatype-summary.js
@@ -0,0 +1,27 @@
+// Primitive
+// 7 type : String , Number , Boolean , null, underfined , Symbol,
+// bigInt
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTeamp = null
+let userEmail;
+
+const id = Symbol('123')
+const antherId = Symbol('123')
+console.log(id === antherId)
+// get fales answer
+
+// Reference (No - primative)
+// Array , Object , Function 
+
+const heros = ["shaktiman", "naagraj","doga" ];
+let myObj = {
+    name : "hitesh",
+    age :23 ,
+
+}
+const myFunction = function(){
+    console.log("Hello world");
+}
